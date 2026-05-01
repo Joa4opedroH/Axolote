fb69a36751d684dd2ed5328a855b8861dba245c4
João Pedro Conceição <165187410+Joa4opedroH@users.noreply.github.com>
2026-05-01 08:57:41 -0300

    Delete tests directory

--- tests/test_scoring.py
+++
@@ -1,9 +0,0 @@
-from skylimpiadas.scoring import software_score
-
-
-def test_software_score_matches_regulation_formula():
-    assert round(software_score(4, 6, 1), 2) == 40.60
-
-
-def test_software_score_allows_zero_progress():
-    assert software_score(0, 0, 0) == 0
